40155f0d34a2d0b656afaeef733a3abf86c29292
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-12 11:01:25 +0200

    Day 4 part 2.

--- src/Day4.kt
+++ src/Day4.kt
@@ -9,12 +9,20 @@ fun main(args: Array<String>) {
     var count = 0
     inputStream.bufferedReader().useLines { lines ->
         lines.forEach { line ->
-            val list = line.split(" ", "\t").map { s -> s.trim() }
-            val set = list.toSet()
-            if (list.size == set.size) {
+            val list = line.split(" ", "\t").map { s -> s.trim().sort() }
+            val all = list
+
+            val set = all.toSet()
+            if (all.size == set.size) {
                 count++
             }
         }
     }
     println(count.toString())
+}
+
+fun String.sort(): String {
+    val toCharArray = this.toCharArray()
+    toCharArray.sort()
+    return String(toCharArray)
 }
