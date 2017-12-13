6e7c97bab0d0340bca778aad1eed9e6240d956a4
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-13 12:51:18 +0200

    Day 9 part 2.

--- src/Day9.kt
+++ src/Day9.kt
@@ -9,35 +9,37 @@ fun main(args: Array<String>) {
     inputStream.bufferedReader().lines().forEach { l ->
         if (!l.startsWith("{")) throw UnsupportedOperationException("Line needs to starts with {")
         println(l)
-        println(processLine(l, 0).toString())
+        println(processLine(l).toString())
     }
 }
 
-fun processLine(l: String, score: Int): Int {
+fun processLine(l: String): Int {
     if (l.isEmpty()) return 0
     val start: Char = l[0]
     when (start) {
-        '{' -> return processLine(l.substring(1), score + 1)
-        '}' -> return score + processLine(l.substring(1), score - 1)
-        '!' -> return processLine(l.substring(2), score)
-        ',' -> return processLine(l.substring(1), score)
+        '{' -> return processLine(l.substring(1))
+        '}' -> return processLine(l.substring(1))
+        '!' -> return processLine(l.substring(2))
+        ',' -> return processLine(l.substring(1))
         '<' -> {
             val garbageEnd = findGarbageEnd(l.substring(1));
-            return processLine(l.substring(garbageEnd + 2), score)
+            return ((garbageEnd.first - garbageEnd.second) + processLine(l.substring(garbageEnd.first + 2)))
         }
         else -> throw UnsupportedOperationException("Unknown [" + start + "]")
     }
 }
 
-fun findGarbageEnd(l: String): Int {
+fun findGarbageEnd(l: String): Pair<Int, Int> {
     var index = 0
+    var canceledCount = 0
     while (l[index] != '>') {
         if (l[index] == '!') {
             index += 2
+            canceledCount += 2
         } else {
             index += 1
         }
     }
-    return index
+    return Pair(index, canceledCount)
 }
 
