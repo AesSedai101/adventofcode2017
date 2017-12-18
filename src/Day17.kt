38e085d6f1cf5583773252f67713392bd644d0b4
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-18 08:04:46 +0200

    Day 17 part 2.

--- src/Day17.kt
+++ src/Day17.kt
@@ -3,12 +3,24 @@
  */
 fun main(args: Array<String>) {
     val input = 304
-    val lock = mutableListOf(0)
-    var index = 0
+   // val lock = mutableListOf(0)
 
-    for(i in 1..2017) {
-        index = (index + input) % lock.size + 1
-        lock.add(index, i)
+    var index = 0
+    var zeroIndex = 0
+    var afterZero = 0
+
+    for(i in 1..50000000) {
+        index = (index + input) % i + 1
+        //lock.add(index, i)
+        if (index == zeroIndex + 1) {
+            afterZero = i
+        }
+        if (index == zeroIndex) {
+            zeroIndex += 1
+        }
     }
-    println(lock.get(index + 1).toString())
+   // println(lock.get(index + 1).toString())
+    //println(lock.get(zeroIndex).toString())
+   // println(lock.get(zeroIndex + 1).toString())
+    println(afterZero.toString())
 }
