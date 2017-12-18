d5a9ba957234379e736f035eaf2ea61a18812285
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-18 14:49:24 +0200

    Day 18 part 2. Still deadlock occasionally, but spits out the correct answer.

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,5 +1,9 @@
 import java.io.File
 import java.io.InputStream
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
 
 /**
  * @author elsabe.ros
@@ -11,44 +15,98 @@ fun main(args: Array<String>) {
         instructions.add(Instr(line))
     }
 
-    println(runInstructions(instructions).toString())
+    val p1 = Program18(0)
+    val p2 = Program18(1)
+
+    thread {
+        p1.runInstructions(p2, instructions)
+        println("0: " + p1.queueCount.toString())
+        p2.stop()
+    }
+    thread {
+        p2.runInstructions(p1, instructions)
+        println("1: " + p2.queueCount.toString())
+        p1.stop()
+    }
 }
 
-fun runInstructions(instructions: List<Instr>): Long {
-    val registers = mutableMapOf<String, Pair<Long, Long>>()
-    var address = 0
+class Program18(val p: Long) {
+    val queue: Queue<Long> = ConcurrentLinkedQueue<Long>()
+    var queueCount = 0;
+    private var waiting : AtomicBoolean = AtomicBoolean(false)
+    private var stopVal : AtomicBoolean = AtomicBoolean(false)
 
-    while(true) {
-        val i = instructions[address]
-        val (value, sound) = registers.getOrDefault(i.register, Pair(0L, 0L))
-        when (i.instruction) {
-            "snd" -> {
-                registers.put(i.register, Pair(value, value))
+    fun runInstructions(other: Program18, instructions: List<Instr>): Long {
+        val registers = mutableMapOf<String, Long>()
+        registers.put("p", p)
+
+        var address = 0
+
+        while (true) {
+            if (stopVal.get()){
+                return -1L
             }
-            "set" -> {
-                registers.put(i.register, Pair(i.parseValue(registers), sound))
-            }
-            "add" -> {
-                registers.put(i.register, Pair(value + i.parseValue(registers), sound))
-            }
-            "mul" -> {
-                registers.put(i.register, Pair(value * i.parseValue(registers), sound))
-            }
-            "mod" -> {
-                registers.put(i.register, Pair(value % i.parseValue(registers), sound))
-            }
-            "rcv" -> {
-                if (sound != 0L) {
-                    return sound
+            val i = instructions[address]
+            val value = registers.getOrDefault(i.register, 0L)
+            when (i.instruction) {
+                "snd" -> {
+                    queue.add(value)
+                    queueCount++
+                }
+                "set" -> {
+                    registers.put(i.register, i.parseValue(registers))
+                }
+                "add" -> {
+                    registers.put(i.register, value + i.parseValue(registers))
+                }
+                "mul" -> {
+                    registers.put(i.register, value * i.parseValue(registers))
+                }
+                "mod" -> {
+                    registers.put(i.register, value % i.parseValue(registers))
+                }
+                "rcv" -> {
+                    waiting.set(true)
+                    val v = other.dequeueValue(this)
+                    waiting.set(false)
+                    if (v == null) {
+                        return -1L
+                    }
+                    registers.put(i.register, v)
+                }
+                "jgz" -> {
+                    val number = i.register.toIntOrNull()
+                    if (number != null) {
+                        if (number > 0) {
+                            address += (i.parseValue(registers) - 1).toInt()
+                        }
+                    } else {
+                        if (value > 0) {
+                            address += (i.parseValue(registers) - 1).toInt()
+                        }
+                    }
                 }
             }
-            "jgz" -> {
-                if (value > 0) {
-                    address += (i.parseValue(registers) - 1).toInt()
-                }
+            address++
+        }
+    }
+
+    private fun dequeueValue(caller: Program18): Long? {
+        while (queue.isEmpty()) {
+            if (caller.check(waiting.get())) {
+                // both are waiting
+                return null
             }
         }
-        address++
+        return queue.poll()
+    }
+
+    private fun check(w: Boolean) : Boolean{
+        return waiting.get() && w && queue.isEmpty()
+    }
+
+    fun stop() {
+        this.stopVal.set(true);
     }
 }
 
@@ -71,10 +129,10 @@ class Instr(line: String) {
         }
     }
 
-    fun parseValue(registers: Map<String, Pair<Long, Long>>): Long {
+    fun parseValue(registers: Map<String, Long>): Long {
         if (numeric) {
             return value.toLong()
         }
-        return registers[value]!!.first
+        return registers[value]!!
     }
 }
