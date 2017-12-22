799c3cff6b276d5d5b0beff152d77c479ca324c9
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-22 08:29:22 +0200

    Day 22 part 2.

--- src/Day22.kt
+++ src/Day22.kt
@@ -11,13 +11,14 @@ fun main(args: Array<String>) {
     val RIGHT = Pair(0, 1)
     val directions = listOf(DOWN, LEFT, UP, RIGHT)
 
-    val map = mutableMapOf<Pair<Int, Int>, Boolean>()
+
+    val map = mutableMapOf<Pair<Int, Int>, BlockState>()
 
     val inputStream: InputStream = File("input/day22.txt").inputStream()
     var rowCount = 0
     inputStream.bufferedReader().lines().forEach { line ->
         for (i in line.toCharArray().indices) {
-            map.put(Pair(rowCount, i), (line[i] == '#'))
+            map.put(Pair(rowCount, i), if(line[i] == '#') BlockState.INFECTED else BlockState.CLEAN)
         }
         rowCount++
     }
@@ -26,21 +27,37 @@ fun main(args: Array<String>) {
     var vDirection = UP
     var infectCount = 0
 
-    repeat(10000, {
-        val infected = map.getOrDefault(vPos, false)
-        if (infected) {
-            // turn right
-            vDirection = directions[(directions.indexOf(vDirection) + 1) % directions.size]
-        } else {
-            // turn left
-            val pos = directions.indexOf(vDirection) - 1 + directions.size
-            vDirection = directions[pos % directions.size]
-            infectCount++
+    repeat(10000000, {
+        val infected = map.getOrDefault(vPos, BlockState.CLEAN)
+        when(infected) {
+            BlockState.CLEAN -> {
+                // turn left
+                val pos = directions.indexOf(vDirection) - 1 + directions.size
+                vDirection = directions[pos % directions.size]
+                map.put(vPos, BlockState.WEAK)
+            }
+            BlockState.WEAK -> {
+                // does not turn
+                infectCount++
+                map.put(vPos, BlockState.INFECTED)
+            }
+            BlockState.INFECTED -> {
+                // turn right
+                vDirection = directions[(directions.indexOf(vDirection) + 1) % directions.size]
+                map.put(vPos, BlockState.FLAGGED)
+            }
+            BlockState.FLAGGED -> {
+                // reverse direction
+                vDirection = directions[(directions.indexOf(vDirection) + 2) % directions.size]
+                map.put(vPos, BlockState.CLEAN)
+            }
         }
-        map.put(vPos, !infected)
         vPos += vDirection
     })
 
     println(infectCount++)
 }
 
+enum class BlockState {
+    CLEAN, WEAK, INFECTED, FLAGGED
+}
