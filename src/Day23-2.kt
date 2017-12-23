c2eaf0b08d999361a51bb00b99ee006ff212fbd9
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-23 17:51:05 +0200

    Day 23-2 solved.

--- src/Day23-2.kt
+++ src/Day23-2.kt
@@ -3,27 +3,14 @@
  */
 fun main(args: Array<String>) {
     var count = 0;
-
     var b = 106700
     val c = 123700
 
-    while (b != c) {
-        val someFlag = determineFlag(b)
-        if (someFlag) {
+    while (b <= c) {
+        if ((2..Math.sqrt(b.toDouble()).toInt()).any { b % it == 0 }) {
             count += 1
         }
         b += 17
     }
     println(count.toString())
-}
-
-private fun determineFlag(b: Int): Boolean {
-    for (d in 2..b) {
-        for (e in 2..b) {
-            if (d * e == b) {
-                return true
-            }
-        }
-    }
-    return false
 }
