66f6e005aac0388e52e8c97f3c05186ab1901d51
Elsabé Ros <elsabe.ros@dariel.co.za>
2017-12-23 17:36:33 +0200

    Day 23-2 basic simplification.

--- src/Day23-2.kt
+++ src/Day23-2.kt
@@ -2,29 +2,15 @@
  * @author elsabe.ros
  */
 fun main(args: Array<String>) {
-    val a = 1
-    var b = 0;
-    var c = 0;
     var d = 0;
     var e = 0;
     var f = 0;
     var g = 0;
     var h = 0;
 
-    // set b 67
-    b = 67
-    //  set c b
-    c = b
-    //  jnz a 2 ->  is not zero
-    //  >-- jnz 1 5 --<
-    //  mul b 100
-    b *= 100
-    //  sub b -100000
-    b -= -100000
-    //  set c b
-    c = b
-    //  sub c -17000
-    c -= -17000
+    var b = 106700
+    val c = 123700
+
     do {
         //  set f 1
         f = 1
@@ -45,8 +31,7 @@ fun main(args: Array<String>) {
                     //  set f 0
                     f = 0
                 }
-                //   sub e -1
-                e -= 1
+                e += 1
                 //   set g e
                 g = e
                 //  sub g b
@@ -63,8 +48,7 @@ fun main(args: Array<String>) {
         } while (g != 0)
         //  jnz f 2
         if (f == 0) {
-            //   sub h -1
-            h -= -1
+            h += 1
         }
         //  set g b
         g = b
@@ -73,10 +57,8 @@ fun main(args: Array<String>) {
         // jnz g 2
         if (g == 0) {
             println(h.toString())
-            //  jnz 1 3
             return
         }
-        //  sub b -17
-        b -= -17
-    } while (1 != 0)//  jnz 1 -23
+        b += 17
+    } while (true)
 }
